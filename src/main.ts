d279894cabe84fa28c05d618064c6443c8b6eb9e
Kamil Nehrybecki <knehrybecki@gmail.com>
2022-05-24 16:04:50 +0200

    corrected socket in  socket

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,61 @@
 import express from 'express'
-const app = express()
 import { createServer } from 'http'
-const server = createServer(app)
 import { Server } from "socket.io"
+
+const app = express()
+const server = createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: 'http://localhost:3001',
     methods: ["GET", "POST"]
   }
 })
 
-io.on('connection', (socket) => {
-  socket.on('new-user', user => {
-    socket.on('room', room => {
-      socket.join(room)
+type Person = {
+  userName: string,
+  roomName: string
+}
 
-      io.to(room).emit('roomMessage', `${user} joined the ${room}`)
+let userData: Person[] = []
 
-      socket.on('chatMessage', (data) => {
-        io.to(room).emit('message', {
-          message: data.message,
-          userName: data.user,
-          clientID: data.clientID
-        })
-      })
+io.on('connection', socket => {
+  socket.on('userData', user => {
+    userData = userData.concat(user)
 
-      socket.on("send-img", (image) => {
-        io.to(room).emit('image', {
-          src: image,
-          userID: socket.id
-        })
-      })
+    socket.join(user.roomName)
 
-      socket.on('disconnect', () => {
-        socket.leave(room)
-        io.to(room).emit('roomMessage', `${user} left the ${room}`)
-      })
+    io.to(user.roomName).emit('roomMessage', `${user.userName} joined the ${user.roomName}`)
+  })
+
+  socket.on('chatMessage', (data) => {
+    const userRoom = userData.map((user) => user.roomName)
+
+    io.to(userRoom).emit('message', {
+      message: data.message,
+      userName: data.user,
+      clientID: data.clientID
     })
+  })
+
+  socket.on("send-img", (image) => {
+    const userRoom = userData.map(user => user.roomName)
+
+    io.to(userRoom).emit('image', {
+      src: image,
+      userID: socket.id
+    })
+  })
+
+  socket.on('disconnect', () => {
+    const userRoom = userData.map(user => user.roomName)
+    const userName = userData.map(user => user.userName)
+
+    socket.leave(userRoom.toString())
+
+    io.to(userRoom).emit('roomMessage', `${userName} left the ${userRoom}`)
+
+    userData = []
   })
 })
 
